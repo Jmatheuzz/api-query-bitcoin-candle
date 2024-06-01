830382b72e95c9550ae34bb018942df20cd6d1bb
Joao Matheus Antunes <you@example.com>
2024-06-01 13:34:59 -0300

    Updates

---
+++ precache-manifest.2967e16ac96637c8a18ca5120c52e106.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75151a7407c1e6f76e9fc5ff050210fe",
+    "url": "/bitcoin-candle/index.html"
+  },
+  {
+    "revision": "d86be26f736e1909fde3",
+    "url": "/bitcoin-candle/static/js/2.0711a6d4.chunk.js"
+  },
+  {
+    "revision": "93d922c9cca44ff73642fac77140d9dd",
+    "url": "/bitcoin-candle/static/js/2.0711a6d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fcc43d2f88b8e885c8d9",
+    "url": "/bitcoin-candle/static/js/main.26464e7c.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/bitcoin-candle/static/js/main.26464e7c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4813cab2521051480eed",
+    "url": "/bitcoin-candle/static/js/runtime-main.edd4bef9.js"
+  }
+]);
